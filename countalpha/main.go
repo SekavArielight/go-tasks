7f81e422566634f5d5e186b7c5d15abcb6d51939
God'swill Sekav Apya <godswillapya@gmail.com>
2026-01-06 22:57:34 +0100

    update test case in main function for CountAlpha to include mixed case letters

--- countalpha/main.go
+++ countalpha/main.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 	fmt.Println(CountAlpha("Hello world"))
-	fmt.Println(CountAlpha("H e l l o"))
+	fmt.Println(CountAlpha("H e l Nl o"))
 	fmt.Println(CountAlpha("H1e2l3l4o"))
 }
